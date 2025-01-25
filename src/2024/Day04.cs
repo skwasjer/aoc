3b01e93ec28453030a92300bea125b499b61afbb
skwasjer <11424653+skwasjer@users.noreply.github.com>
2025-01-25 15:40:11 +0100

    feat(2024-day04): solved

---
+++ src/2024/Day04.cs
@@ -0,0 +1,104 @@
+﻿using System.Numerics;
+using Aoc;
+using Aoc.NUnit;
+using static _2024.Primitives.Compass.CardinalDirection;
+using static _2024.Primitives.Compass.OrdinalDirection;
+
+namespace _2024;
+
+public sealed class Day04 : Puzzle<char[,]>
+{
+    protected override char[,] GetInput()
+    {
+        IEnumerable<string> input = ReadInput();
+
+        string[] data = input.ToArray();
+        char[,] buffer = new char[data[0].Length, data.Length];
+
+        for (int y = 0; y < data.Length; y++)
+        {
+            string line = data[y];
+            for (int x = 0; x < line.Length; x++)
+            {
+                buffer[x, y] = line[x];
+            }
+        }
+
+        return buffer;
+    }
+
+    [Solution(18)]
+    public static int Part1(char[,] input)
+    {
+        Vector2[] directions = [North, South, East, West, NorthEast, NorthWest, SouthEast, SouthWest];
+        return FindChar(input, 'X')
+            .Sum(pos => directions
+                .Sum(direction =>
+                {
+                    bool isMatch = IsWordMatch(input, "XMAS", pos.x, pos.y, direction);
+                    return Convert.ToInt32(isMatch);
+                })
+            );
+    }
+
+    [Solution(9)]
+    public static int Part2(char[,] input)
+    {
+        Vector2[] directions = [NorthEast, NorthWest, SouthEast, SouthWest];
+        return FindChar(input, 'A')
+            .Count(pos =>
+            {
+                var center = new Vector2(pos.x, pos.y);
+                return directions.Sum(direction =>
+                {
+                    Vector2 start = center + direction;
+                    bool isMatch = IsWordMatch(input, "MAS", Convert.ToInt32(start.X), Convert.ToInt32(start.Y), -direction);
+                    return Convert.ToInt32(isMatch);
+                }) == 2; // we're looking for a cross
+            });
+    }
+
+    private static IEnumerable<(int x, int y)> FindChar(char[,] buffer, char findChar)
+    {
+        int xLen = buffer.GetLength(0);
+        int yLen = buffer.GetLength(1);
+
+        for (int y = 0; y < yLen; y++)
+        {
+            for (int x = 0; x < xLen; x++)
+            {
+                char ch = buffer[x, y];
+                if (ch != findChar)
+                {
+                    continue;
+                }
+
+                yield return (x, y);
+            }
+        }
+    }
+
+    private static bool IsWordMatch(char[,] buffer, ReadOnlySpan<char> searchWord, int x, int y, Vector2 direction)
+    {
+        int xLen = buffer.GetLength(0);
+        int yLen = buffer.GetLength(1);
+
+        foreach (char ch in searchWord)
+        {
+            if (x < 0 || x >= xLen || y < 0 || y >= yLen)
+            {
+                return false;
+            }
+
+            if (buffer[x, y] != ch)
+            {
+                return false;
+            }
+
+            x += Convert.ToInt32(direction.X);
+            y += Convert.ToInt32(direction.Y);
+        }
+
+        return true;
+    }
+}
---
+++ src/2024/Primitives/Compass.cs
@@ -0,0 +1,22 @@
+﻿using System.Numerics;
+
+namespace _2024.Primitives;
+
+internal static class Compass
+{
+    internal static class CardinalDirection
+    {
+        public static readonly Vector2 North = new(0, -1);
+        public static readonly Vector2 South = new(0, 1);
+        public static readonly Vector2 East = new(1, 0);
+        public static readonly Vector2 West = new(-1, 0);
+    }
+
+    internal static class OrdinalDirection
+    {
+        public static readonly Vector2 NorthEast = new(1, -1);
+        public static readonly Vector2 NorthWest = new(-1, -1);
+        public static readonly Vector2 SouthEast = new(1, 1);
+        public static readonly Vector2 SouthWest = new(-1, 1);
+    }
+}
